215887e800b74f1b80ac1dce2e5c4e715f41bd83
Dinos Kousidis <konstantinos.kousidis@cern.ch>
2017-03-14 10:33:42 +0100

    serializers: creation of serializers with xslt
    * Adds xslts folder.
      Transformation formats MARC21 to MODS 3.6, 3.5
    				   DublinCore OAI, RDF, SRW
    
    * Adds serializer instances using the xslts.
    
    * Fixes MARCXMLSerializer.dump() when calling without schema_class.
    
    * Adds templates for record export and export options in detail view.
    
    Signed-off-by: Dinos Kousidis <konstantinos.kousidis@cern.ch>

--- invenio_marc21/serializers/__init__.py
+++ invenio_marc21/serializers/__init__.py
@@ -27,10 +27,22 @@
 from __future__ import absolute_import, print_function
 
 from dojson.contrib.to_marc21 import to_marc21
+from invenio_records_rest.serializers.response import (record_responsify,
+                                                       search_responsify)
+from pkg_resources import resource_filename
+
 from .marcxml import MARCXMLSerializer
-from invenio_records_rest.serializers.response import record_responsify, \
-    search_responsify
+
+dublincore_OAI = resource_filename('invenio_marc21',
+                                   'xslts/MARC21slim2OAIDC.xsl')
+dublincore_RDF = resource_filename('invenio_marc21',
+                                   'xslts/MARC21slim2RDFDC.xsl')
+dublincore_SRW = resource_filename('invenio_marc21',
+                                   'xslts/MARC21slim2SRWDC.xsl')
+mods = resource_filename('invenio_marc21', 'xslts/MARC21slim2MODS3-6.xsl')
 
 marcxml_v1 = MARCXMLSerializer(to_marc21)
+dublincore_v1 = MARCXMLSerializer(to_marc21, xslt_filename=dublincore_OAI)
+mods_v1 = MARCXMLSerializer(to_marc21, xslt_filename=mods)
 json_v1_response = record_responsify(marcxml_v1, 'application/json')
 json_v1_search = search_responsify(marcxml_v1, 'application/json')
